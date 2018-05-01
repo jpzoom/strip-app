a147d64fb60b0f48d1021b8f63f70c0373ce6f39
Josip Panic <jolecast44@gmail.com>
2018-05-02 00:18:21 +0200

    neuspjeh sa background slikom

--- resources/views/include/naslov.blade.php
+++ resources/views/include/naslov.blade.php
@@ -1,3 +1,13 @@
-<div class="text-center">
-  <h1>Vjeverix - preglednik stripova</h1>
+<div class ="row  text-center mr-auto">
+    
+    <div class="col-md-3 col-lg-3" style="background-color: #383d41">
+        
+    </div>
+   <div class="navtest col-md-6 col-lg-6 vjeverica" style="background-color:#fafafa">
+    <h1 class="text-center ">Vjeverix - preglednik stripova</h1>
+   </div>
+    <div class="col-md-3 col-lg-3" style="background-color: #383d41">
+        
+    </div>
+    
 </div>
