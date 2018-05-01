2275910bfcae08e696e33b881148d11878a82c67
Josip Panic <jolecast44@gmail.com>
2018-05-02 01:40:25 +0200

    nije vazno

--- resources/views/include/naslov.blade.php
+++ resources/views/include/naslov.blade.php
@@ -3,7 +3,7 @@
     <div class="col-md-3 col-lg-3" style="background-color: #383d41">
         
     </div>
-   <div class="navtest col-md-6 col-lg-6 vjeverica" style="background-color:#fafafa">
+   <div class="navtest col-md-6 col-lg-6" style="background-color:#fafafa">
     <h1 class="text-center ">Vjeverix - preglednik stripova</h1>
    </div>
     <div class="col-md-3 col-lg-3" style="background-color: #383d41">
