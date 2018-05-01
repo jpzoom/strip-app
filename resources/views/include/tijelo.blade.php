54b930b46be6146e8e545ada359113c78ecfb371
Josip Panic <jolecast44@gmail.com>
2018-05-01 20:44:40 +0200

    mala promjena

--- resources/views/include/tijelo.blade.php
+++ resources/views/include/tijelo.blade.php
@@ -2,7 +2,7 @@
 <main role="main" class="container">
       <div class="card jumbotron">
         <h1>Primjer tijela</h1>
-        <p class="lead">Dobrodošli!</p>
+        <p class="lead">Dobrodošli</p>
         @show
       </div>
     </main>
