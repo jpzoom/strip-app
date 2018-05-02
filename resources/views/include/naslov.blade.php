d440658b9a10782a78ad5e4b96150563a88d665a
Josip Panic <jolecast44@gmail.com>
2018-05-02 11:00:56 +0200

    dodani controlleri

--- resources/views/include/naslov.blade.php
+++ resources/views/include/naslov.blade.php
@@ -1,13 +1,13 @@
 <div class ="row  text-center mr-auto">
-    
+
     <div class="col-md-3 col-lg-3" style="background-color: #383d41">
-        
+
     </div>
    <div class="navtest col-md-6 col-lg-6" style="background-color:#fafafa">
-    <h1 class="text-center ">Vjeverix - preglednik stripova</h1>
+    <h1 class="text-center ">Preglednik stripova</h1>
    </div>
     <div class="col-md-3 col-lg-3" style="background-color: #383d41">
-        
+
     </div>
-    
+
 </div>
