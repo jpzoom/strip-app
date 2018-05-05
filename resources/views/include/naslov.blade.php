1a1c81f904e9b0609b33679283c3536b203f07f8
Josip Panic <jolecast44@gmail.com>
2018-05-05 17:52:02 +0200

    www

--- resources/views/include/naslov.blade.php
+++ resources/views/include/naslov.blade.php
@@ -3,8 +3,9 @@
     <div class="col-md-3 col-lg-3" style="background-color: #383d41">
 
     </div>
-   <div class="navtest col-md-6 col-lg-6" style="background-color:#fafafa">
+   <div class="navtest col-md-6 col-lg-6 vje" style="background-image:url({{url('images/header2.png')}})">
     <h1 class="text-center ">Preglednik stripova</h1>
+
    </div>
     <div class="col-md-3 col-lg-3" style="background-color: #383d41">
 
--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #f38606;">
+<nav class="navbar navbar-expand-md navbar-dark"  style="background-color: #b0413e;">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -7,17 +7,17 @@
         <ul class="navbar-nav mr-auto">
 
           <li class="nav-item">
-            <a class="nav-link lead text-light" href="/Preglednik_app/stripapp/public/">Početna</a>
+            <a class="nav-link lead text-light" href="/stripapp/public">Početna</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link lead text-light" href="/Preglednik_app/stripapp/public/insertcomic">Unesi strip</a>
+            <a class="nav-link lead text-light" href="/stripapp/public/insertcomic">Unesi strip</a>
           </li>
 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle lead text-light" data-toggle="dropdown" aria-haspopup="true">Kategorije</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="/Preglednik_app/stripapp/public/category">Kategorije</a>
+              <a class="dropdown-item" href="/stripapp/public/category">Kategorije</a>
               <a class="dropdown-item" href="#">Another action</a>
               <a class="dropdown-item" href="#">Something else here</a>
             </div>
@@ -26,7 +26,7 @@
         </ul>
 
         <li class="navbar-nav nav-item form-inline my-2 my-lg-0 nav-item">
-          <a class="nav-link lead text-light" href="/Preglednik_app/stripapp/public/createprofile">Stvori profil</a>
+          <a class="nav-link lead text-light" href="/stripapp/public/createprofile">Stvori profil</a>
         </li>
 
       </div>
