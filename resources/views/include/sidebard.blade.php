cbc5468ae2e73d3157200efad685aa14371b4c93
Josip Panic <jolecast44@gmail.com>
2018-05-09 15:30:06 +0200

    novi u alone

--- resources/views/include/sidebard.blade.php
+++ resources/views/include/sidebard.blade.php
@@ -1,7 +1,6 @@
 @section('sidebard')
-<div class="card container lined">
-  <br>
-  <h3>API</h3>
-  <p> Ovo je desni sidebar</p>
+<div class="card container lined justify-content-center">
+  <h3 class="card-header row borde text-light txshad">Ostalo</h3>
+  <p><br> Ovo je desni sidebar</p>
   @show <!-- omogućuje prikaz -->
 </div>
